222ec1fd34a897abe59a631edcb4853af3bb65fb
CFGarciaDesing2023 <fabianpalechor96@gmail.com>
2024-09-08 10:22:52 -0500

    Completo

---
+++ js/postRegistroUsuarios.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const registrar = document.getElementById("registrar");
+
+    registrar.addEventListener("click", (e) => {
+        const UsuarioID = document.getElementById("UsuarioID").value;
+        const Email  = document.getElementById("Email").value;
+        const Contraseña = document.getElementById("Contraseña").value;
+        const TipoUsuario= document.getElementById("TipoUsuario").value;
+        const TipoDocumento = document.getElementById("TipoDocumento").value;
+        const NumeroDocumento = document.getElementById("NumeroDocumento").value;
+        const Nombres = document.getElementById("Nombres").value;
+        const Apellidos = document.getElementById("Apellidos").value;
+      
+    
+        const data = {
+            UsuarioID:UsuarioID,
+            Email:Email,
+            Contraseña:Contraseña,
+            TipoUsuario:TipoUsuario,
+            TipoDocumento:TipoDocumento,
+            NumeroDocumento:NumeroDocumento,
+            Nombres: Nombres,
+            Apellidos:Apellidos
+    
+            
+        }
+        
+        fetch("http://ApiSENAProyect2024.somee.com/api/RegistroUsuario", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+          .then((response) => {
+            // Verificar si la respuesta es exitosa (código de estado 200)
+            if (response.ok) {
+              console.log("Datos enviados correctamente");
+
+              window.location.href = "ConsultarUsuarios.html"
+
+            } else {
+              console.error("Error al enviar la solicitud:", response.status);
+            }
+          })
+          .catch((error) => {
+            console.error("Error al enviar la solicitud:", error);
+          });
+      });
+
+
+})
+
---
+++ js/postRegistroVentas.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const registrar = document.getElementById("registrar");
+
+    registrar.addEventListener("click", (e) => {
+        const ClienteID  = document.getElementById("ClienteID").value;
+        const AlojamientoID = document.getElementById("AlojamientoID").value;
+        const FechaVenta= document.getElementById("FechaVenta").value;
+        const TipoVenta = document.getElementById("TipoVenta").value;
+        const Nombre = document.getElementById("Nombre").value;
+        const ValorAPagar = document.getElementById("ValorAPagar").value;
+        const Cantidad = document.getElementById("Cantidad").value;
+        const EstadoVenta  = document.getElementById("EstadoVenta").value;
+      
+    
+        const data = {
+          ClienteID :ClienteID ,
+          AlojamientoID:AlojamientoID,
+          FechaVenta:FechaVenta,
+          TipoVenta:TipoVenta,
+          Nombre:Nombre,
+          ValorAPagar:ValorAPagar,
+          Cantidad:Cantidad ,
+          EstadoVenta :EstadoVenta
+    
+            
+        }
+        
+        fetch("http://ApiSENAProyect2024.somee.com/api/RegistroVentas", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+          .then((response) => {
+            // Verificar si la respuesta es exitosa (código de estado 200)
+            if (response.ok) {
+              console.log("Datos enviados correctamente");
+
+              window.location.href = "../view/index.html"
+
+            } else {
+              console.error("Error al enviar la solicitud:", response.status);
+            }
+          })
+          .catch((error) => {
+            console.error("Error al enviar la solicitud:", error);
+          });
+      });
+
+
+})
+
